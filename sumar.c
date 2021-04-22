441a3478817c25c94a1cc9796899dc33db063d62
Saulo-lopezSu <73849544+Saulo-lopezSu@users.noreply.github.com>
2021-04-21 21:56:10 -0500

    Add files via upload

---
+++ sumar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int x, y, suma;
+	
+	printf("\nIngresa un numero: ");
+	scanf("%d", &x);
+	printf("\nIngresa otro numero: ");
+	scanf("%d", &y);
+	
+	suma=x-y;
+	printf("el resultado de la operacion  es: %d\n", suma);
+	
+	return 0;
+}
+
+
